c27c907f5bf1229a32ae2b8faa3b4b57ca295328
Adrian Corcoran <adrian.corcoran@shopify.com>
2020-05-04 18:02:31 +0100

    Video 17: useContext Part 2

--- src/components/GlobalState.tsx
+++ src/components/GlobalState.tsx
@@ -1,7 +1,67 @@
-import { createContext } from "react";
+import React, { createContext, useReducer } from "react";
 
 export const initialValues = {
   rValue: true,
+  turnOn: () => {},
+  turnOff: () => {},
 };
 
 export const GlobalContext = createContext(initialValues);
+
+type State = {
+  rValue: boolean;
+};
+
+// generic string
+// type Action = {
+//   type: string;
+//   // payload: { //  could also have a payload of data which you can type
+//   //   ...otherstuff
+//   // }
+// };
+
+// explicitly define the action types
+// type Action = {
+//   type: "one" | "two";
+// };
+
+// explicitly define complex action types
+type Action =
+  | { type: "one" }
+  | { type: "two" }
+  | { type: "three"; payload: true }
+  | { type: "four" };
+
+function reducer(state: State, action: Action) {
+  switch (action.type) {
+    case "one":
+      return { rValue: true };
+    case "two":
+      return { rValue: false };
+    default:
+      return state; //  need to have a default in case of unknown action
+  }
+}
+
+export const GlobalProvider: React.FC = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialValues);
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        rValue: state.rValue,
+        turnOn: () => dispatch({ type: "one" }),
+        turnOff: () => dispatch({ type: "two" }),
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
+
+// Global State and Context
+/*
+- we provide the values and tune functions to change them in the global provider
+- the button can then call the functions to change state
+- based on the state rValue, the visibility panel in App.tsx will togglw visibility
+*/
--- src/components/ReducerButtons.tsx
+++ src/components/ReducerButtons.tsx
@@ -1,54 +1,15 @@
 // useReducer allows some Redux style management of state using actions and reducers
 // gives more fine grained control over the state
 
-import React, { useReducer, useRef, useContext } from "react";
+import React, { useRef, useContext } from "react";
 
 import { GlobalContext } from "./GlobalState";
 import { useClickOutside } from "./useClickOutside";
 
-const initialState = { rValue: true };
-
-type State = {
-  rValue: boolean;
-};
-
-// generic string
-// type Action = {
-//   type: string;
-//   // payload: { //  could also have a payload of data which you can type
-//   //   ...otherstuff
-//   // }
-// };
-
-// explicitly define the action types
-// type Action = {
-//   type: "one" | "two";
-// };
-
-// explicitly define complex action types
-type Action =
-  | { type: "one" }
-  | { type: "two" }
-  | { type: "three"; payload: true }
-  | { type: "four" };
-
-function reducer(state: State, action: Action) {
-  switch (action.type) {
-    case "one":
-      return { rValue: true };
-    case "two":
-      return { rValue: false };
-    default:
-      return state; //  need to have a default in case if unknown action
-  }
-}
-
 export const ReducerButtons = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
-
   const ref = useRef<HTMLDivElement>(null!);
 
-  const { rValue } = useContext(GlobalContext);
+  const { rValue, turnOn, turnOff } = useContext(GlobalContext);
 
   // not just expecting a ref, expecting a ref linked to a HTMLDivElement
   useClickOutside(ref, () => {
@@ -59,8 +20,8 @@ export const ReducerButtons = () => {
   return (
     <div ref={ref}>
       {rValue && <h1>Visible</h1>}
-      <button onClick={() => dispatch({ type: "one" })}>Action One</button>
-      <button onClick={() => dispatch({ type: "two" })}>Action Two</button>
+      <button onClick={turnOn}>Action One</button>
+      <button onClick={turnOff}>Action Two</button>
       {/* <button onClick={() => dispatch({ type: "tow" })}>Action Two</button> */}
     </div>
   );
